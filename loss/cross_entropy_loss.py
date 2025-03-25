c23e9b052104bd415c316b3801923f7fb26f4cc8
Babu Pallam <167769818+babupallam@users.noreply.github.com>
2025-03-25 10:49:49 +0000

    Loss functions

--- loss/cross_entropy_loss.py
+++ loss/cross_entropy_loss.py
@@ -1,3 +1,9 @@
-"""
-Auto-generated file.
-"""
+import torch.nn as nn
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self):
+        super(CrossEntropyLoss, self).__init__()
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, outputs, targets):
+        return self.loss_fn(outputs, targets)
--- loss/make_loss.py
+++ loss/make_loss.py
@@ -1,3 +1,27 @@
-"""
-Auto-generated file.
-"""
+from loss.cross_entropy_loss import CrossEntropyLoss
+from loss.triplet_loss import TripletLoss
+
+class CombinedLoss:
+    def __init__(self, loss_fns):
+        self.loss_fns = loss_fns
+
+    def __call__(self, outputs, targets, features=None):
+        total_loss = 0
+        for loss_fn in self.loss_fns:
+            if isinstance(loss_fn, TripletLoss):
+                total_loss += loss_fn(features, targets)
+            else:
+                total_loss += loss_fn(outputs, targets)
+        return total_loss
+
+
+def build_loss(loss_list, num_classes=None, feat_dim=None):
+    loss_fns = []
+
+    if "cross_entropy" in loss_list:
+        loss_fns.append(CrossEntropyLoss())
+
+    if "triplet" in loss_list:
+        loss_fns.append(TripletLoss(margin=0.3))
+
+    return CombinedLoss(loss_fns)
