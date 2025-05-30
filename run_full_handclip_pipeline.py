7d2c2368dfbf9474e24ce176b3e20ef2c831116d
Babu Pallam <167769818+babupallam@users.noreply.github.com>
2025-05-30 06:33:22 +0100

    Update run_full_handclip_pipeline.py

--- run_full_handclip_pipeline.py
+++ run_full_handclip_pipeline.py
@@ -4,27 +4,19 @@ import subprocess
 commands = [
     {
         "title": "🔧 Stage 1: Train Classifier (Frozen Text)",
-        "cmd": "python experiments/stage1_train_classifier_frozen_text/train_stage1_frozen_text.py --config configs/train_stage1_frozen_text/train_vitb16_11k_dorsal_r.yml"
-    },
-    {
-        "title": "🧪 Stage 1: Evaluation",
-        "cmd": "python experiments/stage1_train_classifier_frozen_text/eval_stage1_frozen_text.py configs/eval_stage1_frozen_text/eval_vitb16_11k_dorsal_r.yml"
-    },
-    {
-        "title": "🔧 Stage 2: Train CLIP ReID - Joint Training (Prompt + Image Encoder)",
-        "cmd": "python experiments/stage2_clipreid_integration/train_stage2_joint.py --config configs/train_stage2_clip_reid/train_joint_vitb16_11k_dorsal_r.yml"
-    },
-    {
-        "title": "🧪 Stage 2: Evaluation",
-        "cmd": "python experiments/stage2_clipreid_integration/eval_stage2_joint.py configs/eval_stage2_clip_reid/eval_joint_vitb16_11k_dorsal_r.yml"
-    },
-    {
-        "title": "🔧 Stage 3: Train PromptSG",
         "cmd": "python experiments/stage3_promptsg_integration/train_stage3_promptsg.py --config configs/train_stage3_promptsg/train_stage3_vitb16_11k_dorsal_r.yml"
     },
     {
-        "title": "🧪 Stage 3: Evaluation",
+        "title": "🧪 Stage 1: Evaluation",
+        "cmd": "python experiments/stage1_train_classifier_frozen_text/train_stage1_frozen_text.py --config configs/train_stage1_frozen_text/train_rn50_11k_dorsal_r.yml"
+    },
+    {
+        "title": "🔧 Stage 2: Train CLIP ReID - Joint Training (Prompt + Image Encoder)",
         "cmd": "python experiments/stage3_promptsg_integration/eval_stage3_promptsg.py configs/eval_stage3_promptsg/eval_stage3_vitb16_11k_dorsal_r.yml"
+    },
+    {
+        "title": "🧪 Stage 2: Evaluation",
+        "cmd": "python experiments/stage1_train_classifier_frozen_text/eval_stage1_frozen_text.py configs/eval_stage1_frozen_text/eval_rn50_11k_dorsal_r.yml"
     },
 ]
 
